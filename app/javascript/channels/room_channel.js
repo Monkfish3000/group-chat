508bbde51bc8e5192e41e4850c6a2cf47b2b90b9
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2021-02-23 20:25:09 +0100

    real time messages set up with aciton cable

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 2 },  {
+consumer.subscriptions.create({ channel: "RoomChannel", room_id: 3 },  {
   connected() {
     console.log("Connected...")
     // Called when the subscription is ready for use on the server
@@ -12,6 +12,19 @@ consumer.subscriptions.create({ channel: "RoomChannel", room_id: 2 },  {
 
   received(data) {
     console.log(data)
-    // Called when there's incoming data on the websocket for this channel
-  }
+
+    const element = document.getElementById('user-id');
+    const user_id = Number(element.getAttribute('data-user-id'));
+
+    let html;
+
+    if (user_id === data.message.user_id) {
+      html = data.mine
+    } else {
+      html = data.theirs
+    }
+
+    const messageContainer = document.getElementById('messages')
+    messageContainer.innerHTML = messageContainer.innerHTML + html
+    }
 });
